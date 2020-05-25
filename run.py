e94180e803dcdf488658e5970caa3a068c2100fc
Yoni Schirris <yschirris@gmail.com>
2020-05-25 12:11:22 +0200

    change batch size changes to pass it to all downstream tasks

--- run.py
+++ run.py
@@ -11,16 +11,13 @@ def main():
         gpu_count = torch.cuda.device_count()
         if gpu_count > 1:
             print(f'There are {gpu_count} GPUs with the current setup, so we will increase batch size and later run the model on all GPUs')
-            batch_size = config['batch_size']*gpu_count
+            config['batch_size'] *= gpu_count
         else:
             print("There is only 1 GPU available")
     else:
         print("There are no GPUs available")
-        batch_size = config['batch_size']
 
-
-    dataset = DataSetWrapper(batch_size, **config['dataset'])
-
+    dataset = DataSetWrapper(config['batch_size'], **config['dataset'])
     simclr = SimCLR(dataset, config)
     simclr.train()
 
